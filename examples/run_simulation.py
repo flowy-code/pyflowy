73374b93a13df26a5cab1fd0a3be5c3f4247fe75
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-07 15:24:41 +0000

    examples: run_simulation
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

---
+++ examples/run_simulation.py
@@ -0,0 +1,15 @@
+import flowpy as fpy
+import util
+from pathlib import Path
+
+util.download_file_from_github( Path("./hawaii/input.toml"), relative_file_path="examples/KILAUEA2014-2015/input.toml" )
+util.download_file_from_github( Path("./hawaii/test20m.asc"), relative_file_path="examples/KILAUEA2014-2015/test20m.asc" )
+
+input_folder = Path("/home/moritz/lavaflow/flowy/examples/KILAUEA2014-2015")
+
+input_params = fpy.flowpycpp.parse_config( input_folder/"input.toml" )
+input_params.output_folder = "./output_test"
+input_params.source = input_folder / "test20m.asc"
+
+simulation = fpy.flowpycpp.Simulation(input_params, 1)
+simulation.run()
