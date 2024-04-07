92e678cf4c3ea74bdf4cd3f77d151ffac8c7d678
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-07 14:33:58 +0000

    Examples: create_toy_topographies.py
    This script creates two toy topographies as .asc files. One with a
    constant slope and one saddle.
    
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

---
+++ examples/create_toy_topographies.py
@@ -0,0 +1,41 @@
+import flowpy as fpy
+import numpy as np
+import matplotlib.pyplot as plt
+
+# First we create a topograhy with constant sloe
+x_data = np.linspace(0, 10000, 1000)
+y_data = np.linspace(0, 10000, 1000)
+
+
+def height(x, y):
+    return 5e-2 * x
+
+
+height_data = np.array([[height(x, y) for y in y_data] for x in x_data])
+
+asc_file = fpy.flowpycpp.AscFile()
+asc_file.x_data = x_data
+asc_file.y_data = y_data
+asc_file.height_data = height_data
+asc_file.cell_size = x_data[1] - x_data[0]
+asc_file.save("constant_slope.asc")
+
+
+# Then we create a saddle topography
+x_data = np.linspace(0, 10000, 1000)
+y_data = np.linspace(0, 10000, 1000)
+
+
+def height(x, y, center):
+    return -1e-4 * (x - center[0]) * (y - center[1])
+
+
+center = np.array([np.mean(x_data), np.mean(y_data)])
+height_data = np.array([[height(x, y, center) for y in y_data] for x in x_data])
+
+asc_file = fpy.flowpycpp.AscFile()
+asc_file.x_data = x_data
+asc_file.y_data = y_data
+asc_file.height_data = height_data
+asc_file.cell_size = x_data[1] - x_data[0]
+asc_file.save("saddle.asc")
