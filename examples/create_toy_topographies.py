2cb8b31c37c8707812c34c163ba05e6c564ab2b2
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-18 18:25:39 +0000

    examples: create_toy_topographies flat topography

--- examples/create_toy_topographies.py
+++ examples/create_toy_topographies.py
@@ -39,3 +39,22 @@ asc_file.y_data = y_data
 asc_file.height_data = height_data
 asc_file.cell_size = x_data[1] - x_data[0]
 asc_file.save("saddle.asc")
+
+# Then we create a flat topography
+x_data = np.linspace(0, 50000, 5000)
+y_data = np.linspace(0, 50000, 5000)
+
+
+def height(x, y, center):
+    return 0.0
+
+
+center = np.array([np.mean(x_data), np.mean(y_data)])
+height_data = np.array([[height(x, y, center) for y in y_data] for x in x_data])
+
+asc_file = fpy.flowpycpp.AscFile()
+asc_file.x_data = x_data
+asc_file.y_data = y_data
+asc_file.height_data = height_data
+asc_file.cell_size = x_data[1] - x_data[0]
+asc_file.save("flat.asc")
