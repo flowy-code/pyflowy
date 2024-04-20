4cbd6b6ce16e77b2cfefb7add660c53c8c3cd439
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-20 20:24:15 +0000

    test_line_segment_intersects: more segments

--- examples/test_line_segment_intersects.py
+++ examples/test_line_segment_intersects.py
@@ -3,36 +3,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 lobe = fpy.flowpycpp.Lobe()
-lobe.semi_axes = [8, 2]
-lobe.thickness = 20.0
+lobe.semi_axes = [2, 1]
 lobe.set_azimuthal_angle(np.pi / 4)
-lobe.center = [20, 10]
+lobe.center = [1, 2]
+
 
 def plot_lines_intersect(x1, x2):
-    plt.plot( [x1[0], x2[0]], [x1[1], x2[1]] )
+    plt.plot([x1[0], x2[0]], [x1[1], x2[1]])
 
-    intersection = lobe.line_segment_intersects(x1, x2) 
+    intersection = lobe.line_segment_intersects(x1, x2)
+
     if intersection is None:
         print("no intersection")
-        return
-    
-    p1 = intersection[0]
-    p2 = intersection[1]
-    print("p1= ", p1)
-    print("p2= ", p2)
-    plt.plot( [p1[0], p2[0]], [p1[1], p2[1]], ls="none", marker="o", color="r" )
+        p1 = [float("nan"), float("nan")]
+        p2 = [float("nan"), float("nan")]
+    else:
+        p1 = intersection[0]
+        p2 = intersection[1]
+    print("x1 = [ {}, {} ] ".format(*x1))
+    print("x2 = [ {}, {} ] ".format(*x2))
+    print("p1 = [ {}, {} ] ".format(*p1))
+    print("p2 = [ {}, {} ] ".format(*p2))
+    print("\n")
+    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], ls="none", marker="o", color="r")
 
 
 # extent = lobe.extent_xy()
 
 perimeter = np.array(lobe.rasterize_perimeter(30))
 
-x1 = np.array([0, 5.0])
-x2 = np.array([15, 6])
-
 
 # Plot
 plt.plot(perimeter[:, 0], perimeter[:, 1], color="black")
+
+x1 = lobe.center
+x2 = [2, 2]
+plot_lines_intersect(x1, x2)
+
+x1 = lobe.center
+x2 = [4, 4]
+plot_lines_intersect(x1, x2)
+
+x1 = [-1, -1]
+x2 = [4, 4]
+plot_lines_intersect(x1, x2)
+
+x1 = [-1, -1]
+x2 = [4, -4]
+plot_lines_intersect(x1, x2)
+
+x1 = [0, 2.5]
+x2 = [0.5, 2.5]
+plot_lines_intersect(x1, x2)
+
+x1 = [0, 3.0]
+x2 = [-0.5, 3.0]
 plot_lines_intersect(x1, x2)
 
 plt.show()
