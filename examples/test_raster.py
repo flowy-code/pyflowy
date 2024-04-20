bab7c75a009f53e544a4d2019da047ce421be720
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-20 20:25:14 +0000

    test_raster: formatted

--- examples/test_raster.py
+++ examples/test_raster.py
@@ -2,14 +2,13 @@ import flowpy as fpy
 import numpy as np
 import matplotlib.pyplot as plt
 
-def add_lobe( lobe, topography, new_height_data):
-    intersection_data = topography.compute_intersection( lobe, None )
 
+def add_lobe(lobe, topography, new_height_data):
+    intersection_data = topography.compute_intersection(lobe, None)
 
     for indices, fraction in intersection_data:
         # print("indices={},{}; frac={}\n".format(indices[0], indices[1], fraction))
-        new_height_data[ indices[0], indices[1] ] += fraction * lobe.thickness
-    
+        new_height_data[indices[0], indices[1]] += fraction * lobe.thickness
 
 
 lobe = fpy.flowpycpp.Lobe()
@@ -26,15 +25,13 @@ x_data = np.linspace(0, 40, 40)
 y_data = np.linspace(0, 20, 20)
 height_data = np.zeros(shape=(len(x_data), len(y_data)))
 
-height_data = np.array(
-    [[i + j for j in range(len(y_data))] for i in range(len(x_data))]
-)
+height_data = np.array([[0 for j in range(len(y_data))] for i in range(len(x_data))])
 
 topography = fpy.flowpycpp.Topography(height_data, x_data, y_data)
 
 bbox = topography.bounding_box(lobe.center, extent[0], extent[1])
 
-# Add the lobe 
+# Add the lobe
 # topography.add_lobe(lobe, None)
 new_height_data = np.copy(topography.height_data)
 add_lobe(lobe, topography, new_height_data)
@@ -44,7 +41,7 @@ add_lobe(lobe, topography, new_height_data)
 # Plot
 cell = topography.cell_size()
 # plt.pcolormesh(x_data+0.5*cell, y_data+0.5*cell, topography.height_data.T)
-plt.pcolormesh(x_data+0.5*cell, y_data+0.5*cell, new_height_data.T)
+plt.pcolormesh(x_data + 0.5 * cell, y_data + 0.5 * cell, new_height_data.T)
 plt.axvline(x_data[bbox.idx_x_lower], color="black")
 plt.axvline(x_data[bbox.idx_x_higher], color="black")
 plt.axhline(y_data[bbox.idx_y_lower], color="black")
