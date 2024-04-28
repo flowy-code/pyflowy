833804a213176d85d21d933d3b8a3de9d680a667
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-28 16:39:27 +0000

    BLD: adjust for with_netcdf build option of Flowy

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -62,6 +62,7 @@ PYBIND11_MODULE( flowpycpp, m )
         .def_readwrite( "y_data", &Flowy::AscFile::y_data )
         .def( "__repr__", []( const Flowy::AscFile & ) { return "<AscFile>"; } );
 
+#ifdef WITH_NETCDF
     py::class_<Flowy::NetCDFFile>( m, "NetCDFFile" )
         .def( py::init<>() )
         .def( py::init<Flowy::Topography, Flowy::OutputQuantitiy>() )
@@ -77,6 +78,7 @@ PYBIND11_MODULE( flowpycpp, m )
         .def_readwrite( "x_data", &Flowy::NetCDFFile::x_data )
         .def_readwrite( "y_data", &Flowy::NetCDFFile::y_data )
         .def( "__repr__", []( const Flowy::NetCDFFile & ) { return "<NetCDFFile>"; } );
+#endif
 
     py::class_<Flowy::Lobe>( m, "Lobe" )
         .def( py::init<>() )
